03ce373cf5b0abf8242977f282de8036e6d6adad
Advait Kalakkad <advait.kalakkad@gmail.com>
2021-07-31 17:06:11 -0700

    pagination enabled

--- public/bundle.js
+++ public/bundle.js
@@ -2276,11 +2276,14 @@ app.use((state, emitter) => {
     state.owner = {full_name: null};
     state.contents = [];
     state.current = null;
+    state.page = 1;
+    state.per = null;
+    state.length = null;
 
     emitter.on('navigate', () => {
         console.log(`Navigated to state: ${state.route}`);
 
-        window.fetch(`https://api.are.na/v2/channels/${state.params.slug}`)
+        window.fetch(`https://api.are.na/v2/channels/${state.params.slug}?page=${state.page}`)
         .then(response => {
             return response.json();
         })
@@ -2290,6 +2293,9 @@ app.use((state, emitter) => {
             state.owner = result.owner;
             state.contents = result.contents;
             state.current = result.contents[0].id; 
+            state.length = result.length;
+            state.per = result.per;
+            state.pageSize = Math.ceil(state.length/state.per);
             console.log(state);
     
             emitter.emit('render');
@@ -2298,7 +2304,7 @@ app.use((state, emitter) => {
 
     emitter.on('DOMContentLoaded', () => {
 
-        window.fetch(`https://api.are.na/v2/channels/${state.params.slug}`)
+        window.fetch(`https://api.are.na/v2/channels/${state.params.slug}?page=${state.page}`)
         .then(response => {
             return response.json();
         })
@@ -2309,6 +2315,10 @@ app.use((state, emitter) => {
             state.owner = result.owner;
             state.contents = result.contents;
             state.current = result.contents[0].id;
+            state.length = result.length;
+            state.per = result.per;
+            state.pageSize = Math.ceil(state.length/state.per);
+
             emitter.emit('render');
         });
     });
@@ -2324,6 +2334,50 @@ app.use((state, emitter) => {
 
         emitter.emit('render');
     });
+
+    emitter.on('forward', () => {
+        state.page++;
+
+        window.fetch(`https://api.are.na/v2/channels/${state.params.slug}?page=${state.page}`)
+        .then(response => {
+            return response.json();
+        })
+        .then(result => {
+            console.log(result);
+            
+            state.title = result.title;
+            state.owner = result.owner;
+            state.contents = result.contents;
+            state.current = result.contents[0].id;
+            state.length = result.length;
+            state.per = result.per;
+            state.pageSize = Math.ceil(state.length/state.per);
+            
+            emitter.emit('render');
+        });
+    });
+
+    emitter.on('back', () => {
+        state.page--;
+
+        window.fetch(`https://api.are.na/v2/channels/${state.params.slug}?page=${state.page}`)
+        .then(response => {
+            return response.json();
+        })
+        .then(result => {
+            console.log(result);
+            
+            state.title = result.title;
+            state.owner = result.owner;
+            state.contents = result.contents;
+            state.current = result.contents[0].id;
+            state.length = result.length;
+            state.per = result.per;
+            state.pageSize = Math.ceil(state.length/state.per);
+
+            emitter.emit('render');
+        });
+    });
 });
 
 function mainView(state, emit) {
@@ -2336,12 +2390,25 @@ function mainView(state, emit) {
                     <span class="title">${state.title}</span>
                     <span class="meta">${state.owner.full_name}</span>
                 </div>
+                
                 <div class="list">
                     ${
                         state.contents.map(i => {
                             return listItem(i, emit);
                         })
                     }
+                </div>
+
+                <div class="pagination-bar">
+                    <div onclick="${() => { emit('back') }}" class="pagination-button ${state.page > 1 ? 'on' : 'off'}">
+                        Prev
+                    </div>
+                    <div class="pagination-button page">
+                        Page ${state.page}
+                    </div>
+                    <div onclick="${() => { emit('forward') }}" class="pagination-button ${state.page < state.pageSize ? 'on' : 'off'}">
+                        Next
+                    </div>
                 </div>
             </div>
             <div class="col">
--- public/main.js
+++ public/main.js
@@ -16,11 +16,14 @@ app.use((state, emitter) => {
     state.owner = {full_name: null};
     state.contents = [];
     state.current = null;
+    state.page = 1;
+    state.per = null;
+    state.length = null;
 
     emitter.on('navigate', () => {
         console.log(`Navigated to state: ${state.route}`);
 
-        window.fetch(`https://api.are.na/v2/channels/${state.params.slug}`)
+        window.fetch(`https://api.are.na/v2/channels/${state.params.slug}?page=${state.page}`)
         .then(response => {
             return response.json();
         })
@@ -30,6 +33,9 @@ app.use((state, emitter) => {
             state.owner = result.owner;
             state.contents = result.contents;
             state.current = result.contents[0].id; 
+            state.length = result.length;
+            state.per = result.per;
+            state.pageSize = Math.ceil(state.length/state.per);
             console.log(state);
     
             emitter.emit('render');
@@ -38,7 +44,7 @@ app.use((state, emitter) => {
 
     emitter.on('DOMContentLoaded', () => {
 
-        window.fetch(`https://api.are.na/v2/channels/${state.params.slug}`)
+        window.fetch(`https://api.are.na/v2/channels/${state.params.slug}?page=${state.page}`)
         .then(response => {
             return response.json();
         })
@@ -49,6 +55,10 @@ app.use((state, emitter) => {
             state.owner = result.owner;
             state.contents = result.contents;
             state.current = result.contents[0].id;
+            state.length = result.length;
+            state.per = result.per;
+            state.pageSize = Math.ceil(state.length/state.per);
+
             emitter.emit('render');
         });
     });
@@ -64,6 +74,50 @@ app.use((state, emitter) => {
 
         emitter.emit('render');
     });
+
+    emitter.on('forward', () => {
+        state.page++;
+
+        window.fetch(`https://api.are.na/v2/channels/${state.params.slug}?page=${state.page}`)
+        .then(response => {
+            return response.json();
+        })
+        .then(result => {
+            console.log(result);
+            
+            state.title = result.title;
+            state.owner = result.owner;
+            state.contents = result.contents;
+            state.current = result.contents[0].id;
+            state.length = result.length;
+            state.per = result.per;
+            state.pageSize = Math.ceil(state.length/state.per);
+            
+            emitter.emit('render');
+        });
+    });
+
+    emitter.on('back', () => {
+        state.page--;
+
+        window.fetch(`https://api.are.na/v2/channels/${state.params.slug}?page=${state.page}`)
+        .then(response => {
+            return response.json();
+        })
+        .then(result => {
+            console.log(result);
+            
+            state.title = result.title;
+            state.owner = result.owner;
+            state.contents = result.contents;
+            state.current = result.contents[0].id;
+            state.length = result.length;
+            state.per = result.per;
+            state.pageSize = Math.ceil(state.length/state.per);
+
+            emitter.emit('render');
+        });
+    });
 });
 
 function mainView(state, emit) {
@@ -76,12 +130,25 @@ function mainView(state, emit) {
                     <span class="title">${state.title}</span>
                     <span class="meta">${state.owner.full_name}</span>
                 </div>
+                
                 <div class="list">
                     ${
                         state.contents.map(i => {
                             return listItem(i, emit);
                         })
                     }
+                </div>
+
+                <div class="pagination-bar">
+                    <div onclick="${() => { emit('back') }}" class="pagination-button ${state.page > 1 ? 'on' : 'off'}">
+                        Prev
+                    </div>
+                    <div class="pagination-button page">
+                        Page ${state.page}
+                    </div>
+                    <div onclick="${() => { emit('forward') }}" class="pagination-button ${state.page < state.pageSize ? 'on' : 'off'}">
+                        Next
+                    </div>
                 </div>
             </div>
             <div class="col">
